412789cee101909b45a0c7d012cad74f4b2ec33a
cui7dr <cui7dr@gmail.com>
2020-11-28 20:03:34 +0800

    登录

---
+++ RideHailing/app/src/main/java/com/cui/ridehailing/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.cui.ridehailing.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cui.ridehailing.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
