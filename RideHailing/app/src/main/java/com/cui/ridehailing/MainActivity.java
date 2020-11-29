f27e233530a4b2492dadfa271c0a543b099601b9
cui7dr <cui7dr@gmail.com>
2020-11-29 20:25:07 +0800

    申请权限

--- RideHailing/app/src/main/java/com/cui/ridehailing/MainActivity.java
+++ RideHailing/app/src/main/java/com/cui/ridehailing/MainActivity.java
@@ -2,7 +2,15 @@ package com.cui.ridehailing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+
+import com.cui.ridehailing.activity.LoginActivity;
+
+import java.security.Permission;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Android 6.0+ 动态申请权限
+        if (Build.VERSION.SDK_INT >= 22) {
+            int REQUEST_CODE_CONTACT = 101;
+            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_FINE_LOCATION};
+            //验证是否获取权限
+            for (String str : permissions) {
+                if (this.checkCallingPermission(str) != PackageManager.PERMISSION_GRANTED) {
+                    //申请权限
+                    this.requestPermissions(permissions, REQUEST_CODE_CONTACT);
+                }
+            }
+        }
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
