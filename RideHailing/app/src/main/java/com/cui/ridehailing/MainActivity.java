783755266ffb2de976d69f7d9d3e820024b373e4
cui7dr <cui7dr@gmail.com>
2020-12-04 20:43:22 +0800

    主方法

--- RideHailing/app/src/main/java/com/cui/ridehailing/MainActivity.java
+++ RideHailing/app/src/main/java/com/cui/ridehailing/MainActivity.java
@@ -18,20 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Android 6.0+ 动态申请权限
-        if (Build.VERSION.SDK_INT >= 22) {
-            int REQUEST_CODE_CONTACT = 101;
-            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.ACCESS_FINE_LOCATION};
-            //验证是否获取权限
-            for (String str : permissions) {
-                if (this.checkCallingPermission(str) != PackageManager.PERMISSION_GRANTED) {
-                    //申请权限
-                    this.requestPermissions(permissions, REQUEST_CODE_CONTACT);
-                }
-            }
-        }
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
