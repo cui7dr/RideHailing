403a6b624a7ddfbdcd41232303b4aeb4d29cda09
cui7dr <cui7dr@gmail.com>
2020-12-04 20:40:26 +0800

    登录方法

--- RideHailing/app/src/main/java/com/cui/ridehailing/activity/LoginActivity.java
+++ RideHailing/app/src/main/java/com/cui/ridehailing/activity/LoginActivity.java
@@ -3,10 +3,16 @@ package com.cui.ridehailing.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.cui.ridehailing.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtTxt_account;
+    EditText edtTxt_password;
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
