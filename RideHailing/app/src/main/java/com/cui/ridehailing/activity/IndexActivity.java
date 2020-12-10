ab78dcc7238e1fd318ad800b7c894f8bee1c6444
cui7dr <cui7dr@gmail.com>
2020-12-10 19:52:52 +0800

    首页方法

--- RideHailing/app/src/main/java/com/cui/ridehailing/activity/IndexActivity.java
+++ RideHailing/app/src/main/java/com/cui/ridehailing/activity/IndexActivity.java
@@ -2,12 +2,14 @@ package com.cui.ridehailing.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.cui.ridehailing.R;
 
-public class IndexActivity extends AppCompatActivity {
+public class IndexActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView tv_owner_manage;
     TextView tv_car_manage;
@@ -26,5 +28,36 @@ public class IndexActivity extends AppCompatActivity {
         tv_car_manage = (TextView) findViewById(R.id.tv_car_manage);
         tv_owner_info = (TextView) findViewById(R.id.tv_owner_info);
         tv_car_info = (TextView) findViewById(R.id.tv_car_info);
+        tv_owner_manage.setOnClickListener(this);
+        tv_car_manage.setOnClickListener(this);
+        tv_owner_info.setOnClickListener(this);
+        tv_car_info.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_owner_manage:
+                Intent intent_owner_manage = new Intent(IndexActivity.this,
+                        OwnerManageActivity.class);
+                startActivity(intent_owner_manage);
+                finish();
+                break;
+            case R.id.tv_car_manage:
+                Intent intent_car_manage = new Intent(IndexActivity.this, CarManageActivity.class);
+                startActivity(intent_car_manage);
+                finish();
+                break;
+            case R.id.tv_owner_info:
+                Intent intent_owner_info = new Intent(IndexActivity.this, OwnerInfoActivity.class);
+                startActivity(intent_owner_info);
+                finish();
+                break;
+            case R.id.tv_car_info:
+                Intent intent_car_info = new Intent(IndexActivity.this, CarInfoActivity.class);
+                startActivity(intent_car_info);
+                finish();
+                break;
+        }
     }
 }
