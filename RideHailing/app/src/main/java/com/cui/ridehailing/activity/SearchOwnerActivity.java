182e98401d2645791558c85bab50d44a397159fc
cui7dr <cui7dr@gmail.com>
2020-12-25 21:02:52 +0800

    查询车主信息方法

--- RideHailing/app/src/main/java/com/cui/ridehailing/activity/SearchOwnerActivity.java
+++ RideHailing/app/src/main/java/com/cui/ridehailing/activity/SearchOwnerActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -92,5 +93,15 @@ public class SearchOwnerActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent intent = new Intent(this, IndexActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
